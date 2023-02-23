cbd25e7bd605037c5373ae666dd7cf8659da3736
deeksha-juego <deeksha.s@juegostudioz.com>
2023-02-23 11:26:39 +0530

    fouth commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,8 +14,9 @@ request({url,json:true},(error,{body})=>{
    }
    else
    {
+       
        callback(undefined,
-          body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out.It feels like "+body.current.feelslike+" degrees out"
+          body.current.weather_descriptions[0]+". It is currently "+body.current.temperature+" degrees out.It feels like "+body.current.feelslike+" degrees out.The humidity is "+body.current.humidity+"%."
     )
 
    }
